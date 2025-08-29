e9b6369cdc5e1423fd8987df24d018fbb63d33d7
Beniken Project <a.maldonadobarrera@uandresbello.edu>
2025-08-29 07:40:27 -0400

    aplicacion y cambios

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,17 +1,16 @@
 self.addEventListener("install", (event) => {
-    console.log("Service Worker instalado");
-    event.waitUntil(
-      caches.open("beniken-cache").then((cache) => {
-        return cache.addAll(["/"]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open("beniken-cache").then((cache) => {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
