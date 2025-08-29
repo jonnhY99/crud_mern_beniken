0264e85596541b62265abef7e86ca1c2daab425b
Beniken Project <a.maldonadobarrera@uandresbello.edu>
2025-08-29 07:01:45 -0400

    mejoras para correr apk

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker instalado");
+    event.waitUntil(
+      caches.open("beniken-cache").then((cache) => {
+        return cache.addAll(["/"]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
